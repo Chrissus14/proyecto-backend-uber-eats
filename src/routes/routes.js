9c3f79e00b7f8c66abc13487778b44dd93830274
Christian Gomez <cjgm14@gmail.com>
2020-02-27 22:52:31 -0600

    Ruta para obtener todos los restaurantes por zona

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,24 @@ import { Router } from 'express';
 import data from '../data.json';
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json(data);
-});
+/**
+  const zona = data.find(zona => zona.id === '1');
+  const restaurantes = zona.restaurantes;
+  const restaurant = restaurantes.find(res => res.id === '1');
+  const platillos = restaurant.platillos;
+  const platillo = platillos.find(plat => plat.id === '1');
+ */
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('Recibido');
+router.get('/restaurantes/:zona', (req, res) => {
+  const zonaParam = req.params.zona;
+  // const restaurantes = data.find(zona => zona.zona === zonaParam).restaurantes;
+  if (zonaParam === 'norte') {
+    res.json(data.find(zona => zona.zona === zonaParam).restaurantes);
+  } else if (zonaParam === 'centro') {
+    res.json(data.find(zona => zona.zona === zonaParam).restaurantes);
+  } else {
+    res.json({ error: 'Zona no valida' });
+  }
 });
 
 module.exports = router;
